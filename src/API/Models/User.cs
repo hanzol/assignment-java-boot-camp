d8411d2a9da0056873dec83d9f9b38e374980d34
wetpisit kanpipat <>
2022-04-12 09:56:58 +0700

    add model user * ทดสอบ1

---
+++ src/API/Models/User.cs
@@ -0,0 +1,9 @@
+namespace API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+    }
+}
